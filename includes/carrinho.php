92812e0cd9349a22b4dd3891ad3c77ced371c2f0
jose <jmiguelsaraiva2727@gmail.com>
2024-11-24 14:11:56 +0000

    tird commit

--- includes/carrinho.php
+++ includes/carrinho.php
@@ -6,31 +6,8 @@
     <title>Document</title>
 
     <style>      
-    .cartTab{
-    width: 400px;
-    background-color: #2a2f43f1;
-    color: #eee;
-    position: fixed;
-    top: 0;
-    right: -400px;
-    bottom: 0;
-    display: grid;
-    grid-template-rows: 70px 1fr 70px;
-    transition: .8s;
-    border-radius: 8px;
-    }
-    body.showCart .cartTab{
-    right: 0;
-    }
-    body.showCart .container{
-    transform: translateX(-200px);
-    transition: .8s;
-    }
-    .cartTab .btn{
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    }
-    .cartTab .btn button, .cartTab .btn .checkout-button{
+    
+    .cartTab .close, .cartTab .checkout-button{
     text-decoration: none;
     color: #eee;
     cursor: pointer;
@@ -39,7 +16,7 @@
     border-radius: 4px;
     font-size: 18px;
     }
-    .cartTab .btn button, .cartTab .btn .checkout-button:hover{
+    .cartTab .btn .close, .cartTab .btn .checkout-button:hover{
     background: #BFBFBF;
     transition: .3s;    
     }
@@ -55,7 +32,7 @@
             
         </div>
         <div class="btn ">
-            <button class="close ">CLOSE</button>
+            <a class="close">CLOSE</a>
             <a href="checkout.php" class="checkout-button ">Finalizar Compra</a>
         </div>
     </div>
